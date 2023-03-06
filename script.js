631bea567ff393f00b1ed8f4d929d8336c7642ff
albutti <caique.albutti@hotmail.com>
2023-03-06 14:38:38 -0300

    att

--- script.js
+++ script.js
@@ -6,57 +6,11 @@ function fechar_cv_mikaella(){
 }
 
 
-function abrir_cv_rebecca(){
-    document.getElementById('popup_rebecca').style.display = 'block';
+function abrir_camiseta(){
+    document.getElementById('popup_camiseta').style.display = 'flex';
 }
-function fechar_cv_rebecca(){
-    document.getElementById('popup_rebecca').style.display =  'none';
-}
-
-
-function abrir_cv_alvaro(){
-    document.getElementById('popup_alvaro').style.display = 'block';
-}
-function fechar_cv_alvaro(){
-    document.getElementById('popup_alvaro').style.display =  'none';
+function fechar_camiseta(){
+    document.getElementById('popup_camiseta').style.display =  'none';
 }
 
 
-function abrir_cv_julia(){
-    document.getElementById('popup_julia').style.display = 'block';
-}
-function fechar_cv_julia(){
-    document.getElementById('popup_julia').style.display =  'none';
-}
-
-
-function abrir_cv_murilo(){
-    document.getElementById('popup_murilo').style.display = 'block';
-}
-function fechar_cv_murilo(){
-    document.getElementById('popup_murilo').style.display =  'none';
-}
-
-
-function abrir_cv_fernanda(){
-    document.getElementById('popup_fernanda').style.display = 'block';
-}
-function fechar_cv_fernanda(){
-    document.getElementById('popup_fernanda').style.display =  'none';
-}
-
-
-function abrir_cv_marcus(){
-    document.getElementById('popup_marcus').style.display = 'block';
-}
-function fechar_cv_marcus(){
-    document.getElementById('popup_marcus').style.display =  'none';
-}
-
-
-function abrir_cv_livia(){
-    document.getElementById('popup_livia').style.display = 'block';
-}
-function fechar_cv_livia(){
-    document.getElementById('popup_livia').style.display =  'none';
-}
